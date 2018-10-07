875020f0a37fd128152a14300a1eb81fe73115eb
k <k@ks-Mac.local>
2018-10-07 03:51:51 -0700

    Initial Commit

---
+++ book_xml_parsing_ik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  book_xml_parsing_ik
+//
+//  Created by k on 헤이세이 30. 10. 7..
+//  Copyright © 헤이세이 30년 jik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
